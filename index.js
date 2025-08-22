61de52f618c68c79a4aea9c940d51ab80f14ce5e
muralikrishnaponugoti <ponugotimurali26@gmail.com>
2025-08-22 09:38:47 +0530

    intial Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5dc97cf8cdeb49d0b1152a08c8558652')
+    .then(response=>response.json())
+    .then(data=>{
+        const newsContainer=document.getElementById('newsList');
+        data.articles.forEach((article,index)=>{
+            const newsItem=document.createElement('div');
+            newsItem.classList.add("newsCard");
+            newsItem.id=index;
+            const content=article.content.slice(0,-13);
+            newsItem.innerHTML=`
+            <div>
+                <img src=${article.urlToImage} alt="img"/> 
+            </div>
+            <div>
+                <h2 id="title">${article.title}</h2>
+                <p id="content">${content}</p>
+            </div>
+            `;
+            newsContainer.appendChild(newsItem);
+        })
+
+        const card=document.querySelectorAll(".newsCard");
+        const btn=document.getElementsByClassName("btn");
+        const specific=document.getElementsByClassName("specificNews");
+        const newsListBox=document.getElementsByClassName("newsList");
+        card.forEach(crd => {
+            crd.addEventListener('click',(event)=>{
+                newsListBox[0].style.cssText="display:none;";
+                specific[0].style.cssText="display:flex;";
+                const articleIndex=crd.id;
+                document.getElementById('specificImg').src=data.articles[articleIndex].urlToImage;
+                document.getElementById('specificTitle').innerText=data.articles[articleIndex].title;
+                document.getElementById('specificContent').innerText=data.articles[articleIndex].content.slice(0,-13);
+                btn[0].style.cssText="display:block;"
+            })
+        });
+        btn[0].addEventListener('click',()=>{
+            specific[0].style.cssText="display:none";
+            newsListBox[0].style.cssText="display:flex;";
+            btn[0].style.cssText="display:none;" 
+        })
+
+    })
+    .catch(error=>{
+        console.log(error);
+    })
